c777d924f23adf25970cead74883a7da2e2de357
isabel-ronan <isabelronan1999@gmail.com>
2026-01-30 11:04:16 +0000

    redo-processing-file-and-text-analysis

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - Creating a conda environment for this project is recommended.
 - Required packages can be installed from the `./requirements.txt` file.
 - `requirements.txt` made using the `pip list --format=freeze > requirements.txt` command.
+- `spacy` is used in this project. Run `python -m spacy download en_core_web_sm` after installing packages from `requirements.txt` to prevent errors.
--- nurseNoteProcessing.py
+++ nurseNoteProcessing.py
@@ -1,23 +1,41 @@
+# Import packages.
 import os
-import re
 import json
+import spacy
+import warnings
 import pandas as pd
 
+# Ignore UserWarnings.
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+# Initialize constant variables.
 INPUT_FOLDER = 'data'
 OUTPUT_FOLDER = 'dataProcessed'
 
-def preprocessing(texts):
-    temp_list = []
-    for text in texts:
-        text = re.sub(r"\s+", " ", text).strip()
-        text = re.sub(r"\s+([.,!?;:])", r"\1", text)
-        text = re.sub(r"([.,!?;])\s*", r"\1 ", text)
-        text = text.replace(':`', '')
-        text = text.replace('[', '')
-        text = text.replace(']', '')
-        temp_list.append(text)
-    return temp_list
+# Make Spacy NLP object.
+nlp = spacy.load(
+    "en_core_web_sm",
+    disable=["ner", "parser"]  # speed
+)
 
+# Make function to remove punctuation, make lowercase, remove stopwords, punctuation, lemmatize, remove documents with less than 5 tokens.
+def preprocessing(texts, min_words = 5):
+    cleaned_texts = []
+    for doc in nlp.pipe(texts, batch_size=1000):
+        tokens = [
+            token.lemma_.lower()
+            for token in doc
+            if not token.is_stop
+            and not token.is_punct
+            and token.lemma_ != "-PRON-"
+            and token.is_alpha
+        ]
+        if len(tokens) >= min_words:
+            cleaned_texts.append(" ".join(tokens))
+    return cleaned_texts
+
+# Process all T1 files (this can be adjusted later for T2 also).
 nurse_notes = {}
 for folder in os.listdir(f'./{INPUT_FOLDER}/'):
     if '.' not in folder:
@@ -30,9 +48,11 @@ for folder in os.listdir(f'./{INPUT_FOLDER}/'):
                                 if 'dailyNurseNotes' in file:
                                     nurse_notes[sub_sub_folder.split(' ')[0]] = preprocessing(list(pd.read_excel(f'./{INPUT_FOLDER}/{folder}/{sub_folder}/{sub_sub_folder}/{file}')['Note'].dropna()))
 
+# If saving folder does not exist, make it.
 if not os.path.exists(f'./{OUTPUT_FOLDER}/'):
     os.makedirs(f'./{OUTPUT_FOLDER}/', exist_ok=False)  
 
+# Save JSON output.
 json_str = json.dumps(nurse_notes, indent=4)
 with open(f'./{OUTPUT_FOLDER}/nurseNotes.json', "w") as f:
     f.write(json_str)
